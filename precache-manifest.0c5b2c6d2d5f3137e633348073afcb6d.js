1ae0ea34841583b2fcdcd6259252f055aeaf4b23
MatheusR42 <matheus.araujo.r42@gmail.com>
2020-09-14 23:27:45 -0300

    Updates

---
+++ precache-manifest.0c5b2c6d2d5f3137e633348073afcb6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74899c037e9675c8ee96e5ce65ff60f",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "3da0f4138b240d2ea56e",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1971c52735ac13262cd5",
+    "url": "/react-gh-pages/static/js/2.a6a5f5b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.a6a5f5b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da0f4138b240d2ea56e",
+    "url": "/react-gh-pages/static/js/main.ee3bf2d2.chunk.js"
+  },
+  {
+    "revision": "2cf49062818d06848669",
+    "url": "/react-gh-pages/static/js/runtime-main.c07eba6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
